50b4d34b4a269fa4190f19067ee13f15023851d7
duttamca <duttamca@gmail.com>
2021-02-13 23:02:46 -0500

    added sample jenkins file and groovy script

---
+++ script.groovy
@@ -0,0 +1,12 @@
+def buildApp() {
+    echo 'building the application...'
+}
+
+def testApp() {
+    echo 'testing the application...'
+}
+
+def deployApp() {
+    echo 'deplying the application...'
+    echo "deploying version ${params.VERSION}"
+}
